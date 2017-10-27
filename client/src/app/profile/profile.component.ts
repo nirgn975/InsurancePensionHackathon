2b52265f6c695a58717bc32aea622433142d71a0
nirgn975 <nir@galon.io>
2017-10-27 19:29:03 +0300

    Change to profile scss

--- client/src/app/profile/profile.component.ts
+++ client/src/app/profile/profile.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'iph-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
 
