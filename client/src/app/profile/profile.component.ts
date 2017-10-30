d59e65c6ce7fd83129c25fddf26a5f3226a9e55a
David Bronfen <davidbronfen@gmail.com>
2017-10-30 17:39:14 +0200

    Added haveData variable in order to show/hide the profile or the landing page

--- client/src/app/profile/profile.component.ts
+++ client/src/app/profile/profile.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
+public haveData: Boolean = false;
+
   constructor() { }
 
   ngOnInit() {
