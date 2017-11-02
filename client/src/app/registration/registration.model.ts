d3f5f7c7570518066c8f402c8bb49557ff522edd
David Bronfen <davidbronfen@gmail.com>
2017-11-02 09:15:57 +0200

    Added registration model

---
+++ client/src/app/registration/registration.model.ts
@@ -0,0 +1,9 @@
+export interface Registration {
+  id: string;
+  password: string;
+  passwordConfirmatin: string;
+  cardNumber: number;
+  cvvNumber: number;
+  expMonth: number;
+  expYear: number;
+}
