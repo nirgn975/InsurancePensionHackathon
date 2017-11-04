b4a6038859997ec23e711eb9f2ed153bc20273d5
David Bronfen <davidbronfen@gmail.com>
2017-11-04 16:33:38 +0200

    Dispatch the action and add the registration effect and service to the app module.

--- client/src/app/registration/registration.component.ts
+++ client/src/app/registration/registration.component.ts
@@ -1,7 +1,10 @@
+import { Store } from '@ngrx/store';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { Registration } from './registration.model';
+import * as fromRoot from '../reducers';
+import * as registraionAction from './registration.action';
 
 @Component({
   selector: 'iph-registration',
@@ -12,6 +15,7 @@ export class RegistrationComponent implements OnInit {
 
   months = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'];
   years = ['2017', '2018', '2019', '2020', '2021', '2022'];
+
   registrationForm: FormGroup;
   idControl;
   passwordControl;
@@ -31,7 +35,7 @@ export class RegistrationComponent implements OnInit {
 
   constructor(
     private formBuilder: FormBuilder,
-
+    private store: Store<fromRoot.State>,
   ) {
     this.registrationForm = this.formBuilder.group({
       id: this.formBuilder.control(null, [Validators.minLength(2), Validators.required]),
@@ -50,10 +54,10 @@ export class RegistrationComponent implements OnInit {
     this.cvvControl = this.registrationForm.get('cvv');
     this.expMonthControl = this.registrationForm.get('expMonth');
     this.expYearControl = this.registrationForm.get('expYear');
+
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() { }
 
   checkError(event) {
     // Check if the pressed key is tab, if so we dont want to run validation.
@@ -106,11 +110,6 @@ export class RegistrationComponent implements OnInit {
       expYear: this.registrationForm.value.expYear,
     } as Registration;
 
-    console.log(formModel);
-    // this.store.dispatch(new loginAction.LoginAction(formModel));
+    this.store.dispatch(new registraionAction.RegistrationAction(formModel));
   }
-
-  // TODO registration action flow:
-  // (Dispatch) -> Action -> effect -> service -> reducer -> store
-
 }
