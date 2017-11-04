3eb8369f8d127c2714831f4fe9fe88355113c54f
David Bronfen <davidbronfen@gmail.com>
2017-11-04 12:54:46 +0200

    Registration action have been added.

---
+++ client/src/app/registration/registration.action.ts
@@ -0,0 +1,29 @@
+import { Action } from '@ngrx/store';
+import { Registration } from './registration.model';
+
+export const REGISTRATION =  '[Registration] Registration';
+export const REGISTRATION_SUCCESS = '[Registration] Registration Success';
+export const REGISTRATION_FAILED = '[Registration] Registration Failed';
+
+export class RegistrationAction implements Action {
+  readonly type = REGISTRATION;
+
+  constructor(public payload: Registration) { }
+}
+
+export class RegistrationSuccessAction implements Action {
+  readonly type = REGISTRATION_SUCCESS;
+
+  constructor(public payload: RegistrationResponse) { }
+}
+
+export class RegistrationActionFailed implements Action {
+  readonly type = REGISTRATION_FAILED;
+
+  constructor() { }
+}
+
+export type Actions
+  = RegistrationAction
+  | RegistrationSuccessAction
+  | RegistrationActionFailed;
