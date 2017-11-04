65f5aac662c76d2f23665f792c34e17c04435c19
David Bronfen <davidbronfen@gmail.com>
2017-11-04 13:41:05 +0200

    Registration service has been added.

---
+++ client/src/app/registration/registration.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+
+import { Registration, RegistrationResponse } from './registration.model';
+
+import { environment } from '../../environments/environment';
+
+@Injectable()
+export class RegistrationService {
+
+  constructor(
+    private http: Http,
+  ) { }
+
+  newUserRegistration(registrationData: Registration): Observable<RegistrationResponse> {
+    return this.http.post(`${environment.backend}/api/auth/registration`, registrationData)
+      .map(res => res.json())
+      .catch(this.handleError);
+  }
+
+  private handleError(error: Response) {
+    return Observable.throw(error.json().error || 'Server error');
+  }
+}
