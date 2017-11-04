f548e7105b7edc643c47e78c9916e317c53d4dba
nirgn975 <nir@galon.io>
2017-11-04 15:09:55 +0200

    Upgrade ng-cli

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -1,13 +1,11 @@
 import { TestBed, async } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-
 import { AppComponent } from './app.component';
-
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [ RouterTestingModule ],
-      declarations: [ AppComponent ],
+      declarations: [
+        AppComponent
+      ],
     }).compileComponents();
   }));
 
