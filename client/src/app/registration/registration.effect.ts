2675dd2da1d9579cbc59ece6c0975447ffe60976
David Bronfen <davidbronfen@gmail.com>
2017-11-04 13:03:48 +0200

    Registration  effect have been added.

---
+++ client/src/app/registration/registration.effect.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { Effect, Actions, toPayload } from '@ngrx/effects';
+import { Action } from '@ngrx/store';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
+import 'rxjs/add/operator/switchMap';
+
+import { RegistrationService } from './registration.service';
+import * as registration from './registration.action';
+
+@Injectable()
+export class RegistrationEffects {
+  constructor(
+    private actions$: Actions,
+    private registrationService: RegistrationService,
+  ) { }
+
+  @Effect()
+  registration$: Observable<Action>= this.actions$
+    .ofType(registration.REGISTRATION)
+    .map(toPayload)
+    .switchMap(registrationInfo => this.registrationService.registration(registrationInfo)
+      .map(registrationData => new registration.RegistrationSuccessAction(registrationData))
+      .catch(() => Observable.of({ type: 'REGISTRATION_FAILED' }))
+    );
+}
