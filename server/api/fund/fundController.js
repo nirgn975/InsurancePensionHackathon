098cbc9a77a683b48ae71c538c4f2f0e3bca5f13
David Bronfen <davidbronfen@gmail.com>
2017-11-06 17:21:56 +0200

    Return data or the dates that user have to wait to get the data.

--- server/api/fund/fundController.js
+++ server/api/fund/fundController.js
@@ -1,11 +1,19 @@
 const Fund = require('./fundModel');
 
 exports.ownData = (req, res, next) => {
+  const timeDiff = Math.abs(req.user.dates.registrationDate.getTime() - req.user.dates.expectedDataDate.getTime());
+  const diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+
   Fund.find({ user: req.user._id })
     .select(['-_id', '-__v', '-kind'])
     .exec()
     .then((fundData) => {
-      res.json({ own_data: fundData.length !== 0 });
+
+      if(fundData.length !== 0) {
+        res.json({ own_data: fundData });
+      }
+
+      res.json({ own_data: diffDays });
     }, (error) => {
       res.json(error);
     });
