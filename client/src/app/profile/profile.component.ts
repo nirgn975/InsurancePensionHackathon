fcaa3aa57ff33428b33924cd73603b4a68da0ab0
David Bronfen <davidbronfen@gmail.com>
2017-11-06 20:57:42 +0200

    Added dispatch to the profile component initializer

--- client/src/app/profile/profile.component.ts
+++ client/src/app/profile/profile.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+
+import * as fromRoot from '../reducers';
+import * as profileAction from './profile.action';
 
 @Component({
   selector: 'iph-profile',
@@ -7,17 +11,23 @@ import { Router } from '@angular/router';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
-  public haveData: Boolean = true;
+  public haveData: Boolean = false;
 
   constructor(
     private router: Router,
+    private store: Store<fromRoot.State>,
   ) { }
 
 
   ngOnInit() {
-    if (this.haveData) {
-      this.router.navigate(['profile/bond']);
-    }
+    this.store.dispatch(new profileAction.ProfileAction());
+    // this.store.select(fromRoot.getProfileState).subscribe(
+    //   res => {
+    //     console.log(res);
+    //   }
+    // );
+    // if (this.haveData) {
+    //   this.router.navigate(['profile/bond']);
+    // }
   }
-
 }
