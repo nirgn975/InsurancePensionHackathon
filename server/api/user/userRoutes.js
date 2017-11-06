dfdec7dc70a3d02211b81984835c8c98ac42125d
David Bronfen <davidbronfen@gmail.com>
2017-11-06 12:14:35 +0200

    Remove unnecessary code

--- server/api/user/userRoutes.js
+++ server/api/user/userRoutes.js
@@ -8,7 +8,7 @@ router.get('/me', [auth.decodeToken(), auth.getFreshUser()], controller.me);
 
 router.route('/')
   .get(controller.get)
-  .post(controller.expectedDate, controller.post);
+  .post(controller.post);
 
 router.route('/:id')
   .get(controller.getOne)
